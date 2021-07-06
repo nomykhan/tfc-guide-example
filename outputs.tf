d3ce5a67bd18d0f61d34647f3d3b3220f16f1ce8
nomykhan <52381359+nomykhan@users.noreply.github.com>
2021-07-06 11:16:19 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,24 @@
-output "tfc_example_table_arn" {
-  value = aws_dynamodb_table.tfc_example_table.arn
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "instance_host_id" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.host_id
+}
+
+output "instance_primary_network_interface_id" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.primary_network_interface_id
+}
+
+output "instance_password_data" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.password_data
 }
